8b96429c9cebeea943e7b474b0edf400ddef7fcd
abelmark <mabel16113@gmail.com>
2018-03-01 23:07:32 -0500

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+
+if (process.env.NODE_ENV === 'production'){
+
+} else {
+
+}
+module.exports = {
+  googleClientId: "267491162947-n4q2tmljajflrdmjdl14d3hhgsv7o5i7.apps.googleusercontent.com",
+  googleClientSecret: "FzbLUruUf1XXCTGPwkavida_",
+  mongoURI: "mongodb://mark:password@ds123752.mlab.com:23752/postmando-dev",
+  cookieKey: "asdfasduf08983120s9adf90uawef;asdofj1#@as0df9asdfja"
+}
