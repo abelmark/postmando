68c4d2f3b3abeb9e3403c11f869fbdd166504bc6
abelmark <mabel16113@gmail.com>
2018-03-13 15:07:14 -0400

    removed comments

--- client/src/index.js
+++ client/src/index.js
@@ -18,6 +18,3 @@ ReactDOM.render(
   </Provider>, 
   document.getElementById('root')
 );
-
-console.log('Stripe key is', process.env.REACT_APP_STRIPE_KEY);
-console.log('Environment is ', process.env.NODE_ENV)
