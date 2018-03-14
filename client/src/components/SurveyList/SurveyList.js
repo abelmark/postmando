e78bae8a9f4add7a13f39d62f514ec631c1320e0
abelmark <mabel16113@gmail.com>
2018-03-13 20:54:29 -0400

    pre-d3

--- client/src/components/SurveyList/SurveyList.js
+++ client/src/components/SurveyList/SurveyList.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Transition } from "react-transition-group";
 import { fetchSurveys } from "../../actions/index";
+
+// import classes from "../SurveyList.css";
 
 class SurveyList extends Component {
   componentWillMount() {}
@@ -10,18 +13,45 @@ class SurveyList extends Component {
   }
 
   renderSurveys() {
+    const duration = 300;
+
+    const defaultStyle = {
+      transition: `opacity ${duration}ms ease-in-out`,
+      opacity: 0
+    };
+
+    const transitionStyles = {
+      entering: { opacity: 0 },
+      entered: { opacity: 1 }
+    };
+
+    const Card = ({ in: inProp }) => (
+      <Transition in={inProp} timeout={duration}>
+        {state => (
+          <div
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state]
+            }}
+          >
+
+          </div>
+        )}
+      </Transition>
+    );
+    
     return this.props.surveys.reverse().map(({ body, title, yes, no }) => {
       return (
-        <div className="card white darken-1">
-          <div className="card-content">
-            <span className="card-title">{title}</span>
-            <p>{body}</p>
+          <div className="card white darken-1">
+            <div className="card-content">
+              <span className="card-title">{title}</span>
+              <p>{body}</p>
+            </div>
+            <div className="card-action">
+              <a href="#">yes: {yes}</a>
+              <a href="#">no: {no}</a>
+            </div>
           </div>
-          <div className="card-action">
-            <a href="#">yes: {yes}</a>
-            <a href="#">no: {no}</a>
-          </div>
-        </div>
       );
     });
   }
@@ -31,7 +61,7 @@ class SurveyList extends Component {
       <div>
         <h5>Surveys</h5>
         <div className="row">
-          <div className="col lg-12">{this.renderSurveys()}</div>
+          <div>{this.renderSurveys()}</div>
         </div>
       </div>
     );
